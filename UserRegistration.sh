4d1b87e60a086c8dc58c6837607f5918234538a1
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-26 20:38:59 +0530

    Enter Last name with Cap

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,11 +1,20 @@
 #! /bin/bash -x
 
+#Enter First Name with Cap"
 echo "Enter First name:"
 read Firstname
 
+#Enter Lastname with Cap
+echo "Enter last name:"
+read Lastname
+
+#Pattern for FirstName 
 FirstNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
-if [[ $Firstname =~ $FirstNamePat ]]
+#Pattern for Lastname
+LastNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $Firstname =~ $FirstNamePat && $Lastname =~ $LastNamePat ]]
 then
         echo "yes"
 else
