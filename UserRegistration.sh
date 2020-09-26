d881d41d97040c6a83e92d668a605186089ff534
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-26 20:46:49 +0530

    Enter Mobile Number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -12,6 +12,10 @@ read Lastname
 echo "Enter Email Address :"
 read Email
 
+#User Mobile No
+echo "Enter valid Mobile Number:"
+read MobileNo
+
 #Pattern for FirstName 
 FirstNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
@@ -21,7 +25,10 @@ LastNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
 #pattern for Email address
 EmailPat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 
-if [[ $Firstname =~ $FirstNamePat && $Lastname =~ $LastNamePat &&  $Email =~ $EmailPat ]]
+#pattern for MobileNo
+MobileNoPat="^([9]{1}[1][[:space:]]{1}|[0]{1})?[6-9]{1}[0-9]{9}$"
+
+if [[ $Firstname =~ $FirstNamePat && $Lastname =~ $LastNamePat &&  $Email =~ $EmailPat && $MobileNo =~ $MobileNoPat ]]
 then
         echo "yes"
 else
