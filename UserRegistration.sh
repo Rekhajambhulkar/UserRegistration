8a7fc664b193ee752003ff1c5a6ba6ef8e4916ff
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-26 20:42:29 +0530

    Enter Email Address

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -8,13 +8,20 @@ read Firstname
 echo "Enter last name:"
 read Lastname
 
+#Enter User email address
+echo "Enter Email Address :"
+read Email
+
 #Pattern for FirstName 
 FirstNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
 #Pattern for Lastname
 LastNamePat="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
-if [[ $Firstname =~ $FirstNamePat && $Lastname =~ $LastNamePat ]]
+#pattern for Email address
+EmailPat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+
+if [[ $Firstname =~ $FirstNamePat && $Lastname =~ $LastNamePat &&  $Email =~ $EmailPat ]]
 then
         echo "yes"
 else
